ffd1f6ea614401f26cfa73b06982cbaddb581f05
StephaneLP <stephanelepajolec@outlook.com>
2023-04-20 22:31:34 +0200

    maj

--- db/mock-coworkings.js
+++ db/mock-coworkings.js
@@ -4,7 +4,7 @@ const coworkings = [
         name: "Imagin'Office",
         price: { "hour": null, "day": 25, "month": 199 },
         address: { "number": "7", "street": "place des Citernes", "postCode": 33800, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://image-krusty.galaxy.tf/wijpeg-bkt720gqomr3nyabsfmsicyo4/imagin-office-bordeaux0006_standard.jpg?crop=49%2C0%2C788%2C591&width=1844",
         superficy: 1400,
         capacity: 122,
         created: new Date()
@@ -14,7 +14,7 @@ const coworkings = [
         name: "Anticafé",
         price: { "hour": 5, "day": 23, "month": 235 },
         address: { "number": "11", "street": "rue Duffour Dubergier", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://www.anticafe.eu/wp-content/uploads/2018/10/Anticafe%CC%81Bordeaux-8-1024x713.jpg",
         superficy: 95,
         capacity: 45,
         created: new Date()
@@ -24,7 +24,7 @@ const coworkings = [
         name: "Oasis Coworking",
         price: { "hour": 4, "day": 21, "month": 100 },
         address: { "number": "68bis", "street": "avenue Jean Jaurès", "postCode": 33150, "city": "Cenon" },
-        picture: "",
+        picture: "https://oasis-coworking.com/wp-content/uploads/2018/05/espace-coworking-oasis-cenon-flexible.jpg",
         superficy: 200,
         capacity: 27,
         created: new Date()
@@ -34,7 +34,7 @@ const coworkings = [
         name: "WIN Mériadeck",
         price: { "hour": null, "day": 30, "month": null },
         address: { "number": "61", "street": "rue du Château d'Eau", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://w-in.fr/wp-content/uploads/2022/09/WiN_MERIADECK_%C2%A9Vincent-Bengold_002_min-1024x683-1.jpeg",
         superficy: 1850,
         capacity: 130,
         created: new Date()
@@ -44,7 +44,7 @@ const coworkings = [
         name: "Start-Way",
         price: { "hour": 6, "day": 36, "month": null },
         address: { "number": "39", "street": "rue du Château d'Eau", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://www.start-way.com/wp-content/uploads/2022/11/Espace-de-coworking-a-Bordeaux-26.jpeg",
         superficy: 300,
         capacity: 253,
         created: new Date()
@@ -54,7 +54,7 @@ const coworkings = [
         name: "Locomotiv'",
         price: { "hour": null, "day": 40, "month": 300 },
         address: { "number": "4", "street": "cours de l'Intendance", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://media.abcsalles.com/images/1/salles/1440x960/654814/locomotiv-20.jpg",
         superficy: 600,
         capacity: 136,
         created: new Date()
@@ -64,7 +64,7 @@ const coworkings = [
         name: "Digital Village",
         price: { "hour": null, "day": 30, "month": null },
         address: { "number": "88", "street": "rue Judaïque", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://s3-eu-west-1.amazonaws.com/privateaser-media/etab_photos/40450/1500x750/288757.jpg",
         superficy: 150,
         capacity: 52,
         created: new Date()
@@ -74,7 +74,7 @@ const coworkings = [
         name: "WIGI - Grands-Hommes",
         price: { "hour": 4.8, "day": 24, "month": 240 },
         address: { "number": "26", "street": "rue Condillac", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://image-krusty.galaxy.tf/wijpeg-cx9gtukn3l7qs3bb9ula5kp9m/wigi-coworking-bordeau06_standard.jpg?crop=89%2C0%2C1423%2C1067&width=1280",
         superficy: 200,
         capacity: 205,
         created: new Date()
@@ -84,7 +84,7 @@ const coworkings = [
         name: "Mota",
         price: { "hour": null, "day": 24, "month": 320 },
         address: { "number": "13", "street": "rue de la Manutention", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://medias.bureauxlocaux.com/images/annonce/normal/3128/20201204_07195102992b74bfdd1024e04c11e896f8b71d.jpg",
         superficy: 400,
         capacity: 50,
         created: new Date()
@@ -94,7 +94,7 @@ const coworkings = [
         name: "WIGI - Wilson",
         price: { "hour": 4.8, "day": 24, "month": 240 },
         address: { "number": "71", "street": "boulevard du Président Wilson", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://www.wicofi.fr/wp-content/uploads/2022/05/wigi-coworking-bordeaux-salle-reunion.jpeg",
         superficy: 220,
         capacity: 152,
         created: new Date()
@@ -104,7 +104,7 @@ const coworkings = [
         name: "Ocube",
         price: { "hour": null, "day": 33, "month": 330 },
         address: { "number": "55", "street": "rue Saint Joseph", "postCode": 33000, "city": "Bordeaux" },
-        picture: "",
+        picture: "https://image-krusty.galaxy.tf/wijpeg-avq4ayhvujyh1hl9i5lgx3vlc/ocube-coworking-bordeaux-67_standard.jpg?crop=111%2C0%2C1779%2C1334&width=1844",
         superficy: 600,
         capacity: 50,
         created: new Date()
